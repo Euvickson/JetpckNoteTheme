99b472e4f07e1d76d3a20eea952b6ea3501ad120
Euvickson <victor.eugennio@gmail.com>
2023-10-05 13:46:06 -0300

    Created the Textfield component and the NoteScreenButton, and have done the logic to change the values of the TextFields

--- app/src/main/java/br/com/euvickson/jetpcknotetheme/screen/NoteScreen.kt
+++ app/src/main/java/br/com/euvickson/jetpcknotetheme/screen/NoteScreen.kt
@@ -1,7 +1,7 @@
 package br.com.euvickson.jetpcknotetheme.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
@@ -11,22 +11,62 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.euvickson.jetpcknotetheme.R
+import br.com.euvickson.jetpcknotetheme.components.NoteButton
+import br.com.euvickson.jetpcknotetheme.components.NoteInputText
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteScreen() {
+
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
     Column (modifier = Modifier.padding(6.dp)){
         TopAppBar(
             title = { Text(text = stringResource(id = R.string.app_name)) },
             actions = { Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "Icon")},
             colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = Color(0xFFDADFE3))
         )
+
+        //Content
+        Column (modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+            NoteInputText(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) title = it
+                }
+            )
+
+            NoteInputText(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) description = it
+                }
+            )
+
+            NoteButton(text = "Save", onClick = { })
+        }
+
     }
 }
 
