b1cb5a924ce68632b43c5a1adeb59e5708ef0dc0
Euvickson <victor.eugennio@gmail.com>
2023-10-09 13:31:00 -0300

    ViewModel Implemented in the project

--- app/src/main/java/br/com/euvickson/jetpcknotetheme/MainActivity.kt
+++ app/src/main/java/br/com/euvickson/jetpcknotetheme/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.euvickson.jetpcknotetheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,9 +14,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import br.com.euvickson.jetpcknotetheme.data.NoteDataSource
 import br.com.euvickson.jetpcknotetheme.model.Note
 import br.com.euvickson.jetpcknotetheme.screen.NoteScreen
+import br.com.euvickson.jetpcknotetheme.screen.NoteViewModel
 import br.com.euvickson.jetpcknotetheme.ui.theme.JetpckNoteThemeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,20 +31,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val notes = remember { mutableStateListOf<Note>() }
-                    NoteScreen(
-                        notes = notes,
-                        onAddNote = {
-                            notes.add(it)
-                        },
-                        onRemoveNote = {
-                            notes.remove(it)
-                        }
-                    )
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel)
+
                 }
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+    NoteScreen(
+        notes = notesList,
+        onAddNote = { noteViewModel.addNote(it) },
+        onRemoveNote = { noteViewModel.removeNote(it) }
+    )
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/br/com/euvickson/jetpcknotetheme/screen/NoteViewModel.kt
+++ app/src/main/java/br/com/euvickson/jetpcknotetheme/screen/NoteViewModel.kt
@@ -1,4 +1,27 @@
 package br.com.euvickson.jetpcknotetheme.screen
 
-class NoteViewModel {
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import br.com.euvickson.jetpcknotetheme.data.NoteDataSource
+import br.com.euvickson.jetpcknotetheme.model.Note
+
+class NoteViewModel : ViewModel() {
+    private var noteList = mutableStateListOf<Note>()
+
+    init {
+        noteList.addAll(NoteDataSource().loadNotes())
+    }
+
+    fun addNote(note: Note) {
+        noteList.add(note)
+    }
+
+    fun removeNote(note: Note) {
+        noteList.remove(note)
+    }
+
+    fun getAllNotes():List<Note> {
+        return noteList
+    }
+
 }
